5a64dc64857922536c39cf50987fe70ce7d6b58b
rishichawla <rishi.chawla14@gmail.com>
2020-04-28 02:49:07 +0530

    login via database

--- authService/authService.py
+++ authService/authService.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect
 import json
+import psycopg2
 
 app = Flask(__name__)
 
@@ -15,13 +16,42 @@ def generateUsers(n):
     with open('users.json', 'w') as outfile:
         json.dump(users, outfile, indent=2)
 
+def config(filename='database.ini', section='postgresql'):
+    from configparser import ConfigParser
+
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+
+params = config()
+
 def checkLogin(username, password):
-    f = open('users.json')
+    try:
+        print('Connecting to the PostgreSQL database...')
+        connection = psycopg2.connect(**params)
 
-    data = json.load(f)
+        cur = connection.cursor()
+        cur.execute('SELECT password from login where username = %s', (username,))
 
-    if data[username] == password:
-        return True    
+        row = cur.fetchone()
+
+        if row is None:
+            return False
+        
+        if password == row[0]:
+            return True
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
     return False
 
 @app.route('/')
@@ -34,7 +64,6 @@ def login():
         if checkLogin(request.form['username'], request.form['pass']):
             return redirect('http://localhost:5001/')
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     generateUsers(5)
