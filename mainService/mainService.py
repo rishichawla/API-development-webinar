db37810af4d34764fb0a7e50d8b021d6fda04287
rishichawla <rishi.chawla14@gmail.com>
2020-04-28 15:37:37 +0530

    GET and POST request

--- mainService/mainService.py
+++ mainService/mainService.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -12,5 +12,17 @@ def getStats():
     response = json.loads(response.text)
     return render_template('index.html', countries=response['Countries'][1:11])
 
+@app.route('/country', methods=["GET"])
+def getCountryStats():
+    response = requests.get(url + 'total/country/' + request.args.get('country'))
+    response = json.loads(response.text)
+
+    confirmedCases = {}
+    for data in response:
+        confirmedCases[data['Date'][:10]] = data['Confirmed']
+
+
+    return render_template('country.html', country = request.args.get('country'), confirmedCases = confirmedCases)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
